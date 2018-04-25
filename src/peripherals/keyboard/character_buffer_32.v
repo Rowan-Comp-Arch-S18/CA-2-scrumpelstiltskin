824f1bbdda4c738ddefdc77b87acaa7e58ebf62a
John McAvoy <mcavoyj5@students.rowan.edu>
2018-04-25 11:34:09 -0700

    FIxes more typos in character_buffer

--- src/peripherals/keyboard/character_buffer_32.v
+++ src/peripherals/keyboard/character_buffer_32.v
@@ -1,9 +1,10 @@
 module character_buffer_32(PS2_data, PS2_clk, system_clk, read, reset, out);
     input read;
     input system_clk;
-    input r;
     input PS2_clk;
     input PS2_data;
+	 input reset;
+	 
     output [7:0] out;
 
     reg [4:0] first_addr, last_addr;
@@ -30,36 +31,36 @@ module character_buffer_32(PS2_data, PS2_clk, system_clk, read, reset, out);
         last_addr <= 5'b0;
     end
 
-    always @(posedge clk or posedge reset or posedge PS2_clk or posedge read) begin
-        if(!clk && !reset && !err && PS2_clk) begin
+    always @(posedge system_clk or posedge reset or posedge PS2_clk or posedge read) begin
+        if(!system_clk && !reset && !err && PS2_clk) begin
             if(input_counter == 4'b0) begin
                 if(PS2_data != 1'b0)
                     err <= 4'b0;
                 else begin
                     write <= 1'b0;
-                    last_addr = address_select + 5'b1;
-                    input_buffer <= 5'b0;
+                    last_addr = last_addr + 5'b1;
+                    input_character_buffer <= 5'b0;
                 end
 
             end
             else if (input_counter == 4'd10) begin
                 if(PS2_data != 1'b1)
-                    clear_buffer <= 1'b1;
+                    err <= 1'b1;
                 write <= 1'b1;
             end
             else if (input_counter == 4'd9) begin
                     err <= ~(input_character_buffer[7] ^ input_character_buffer[6] ^ input_character_buffer[5] ^ input_character_buffer[4] ^ input_character_buffer[3] ^ input_character_buffer[2] ^ input_character_buffer[1] ^ input_character_buffer[0]) == PS2_data;
             end
             else begin
-                input_buffer <= input_buffer >> 1;
-                input_buffer = input_buffer + {PS2_data, 9'b0};
+                input_character_buffer <= input_character_buffer >> 1;
+                input_character_buffer <= input_character_buffer + {PS2_data, 9'b0};
             end
         end
 
-        if(clear_buffer) begin
+        if(err) begin
             input_counter <= 4'b0;
             write <= 1'b0;
-            clear_buffer <= 1'b0;
+            err <= 1'b0;
         end
 
         if(read) begin
