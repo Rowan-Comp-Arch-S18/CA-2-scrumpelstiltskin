f36fd37c5b809608896a5ceca75e8ff2185f94cd
John McAvoy <mcavoyj5@students.rowan.edu>
2018-05-01 17:54:50 -0400

    updates keyboard_testbench

--- src/peripherals/keyboard/keyboard_testbench.v
+++ src/peripherals/keyboard/keyboard_testbench.v
@@ -1,7 +1,6 @@
 module keyboard_testbench();
 
     parameter message = 96'h48_65_6c_6c_6f_20_57_6f_72_6c_64_21;
-    reg [6:0] message_counter;
     reg [13:0]address;
     reg PS2_data;
     reg PS2_clk;
@@ -14,13 +13,39 @@ module keyboard_testbench();
     keyboard dut00(address, PS2_data, PS2_clk, system_clk, data, reset);
 
     initial begin
-        message_counter <= 7'b0;
         address <= 14'h3fff;
         reset <= 1'b0;
         system_clk <= 1'b1;
         PS2_clk <= 1'b0;
         PS2_clk <= 1'b0;
 
+        #20
+        PS2_data <= 1'b0;
+
+        #20
+        PS2_data <= 1'b0;
+        #20
+        PS2_data <= 1'b0;
+        #20
+        PS2_data <= 1'b0;
+        #20
+        PS2_data <= 1'b1;
+        #20
+        PS2_data <= 1'b0;
+        #20
+        PS2_data <= 1'b0;
+        #20
+        PS2_data <= 1'b1;
+        #20
+        PS2_data <= 1'b0;
+
+        #20
+        PS2_data <= 1'b1;
+
+        #20
+        PS2_data <= 1'b1;
+
+        $stop;
     end
 
     always begin
@@ -32,49 +57,5 @@ module keyboard_testbench();
         #10
         PS2_clk <= ~PS2_clk;
     end
-
-    always begin
-
-        message_counter <= 7'b0;
-        #20
-        PS2_data <= 1'b0;
-
-        #20
-        PS2_data <= message[message_counter];
-        message_counter <= message_counter + 7'b1;
-        #20
-        PS2_data <= message[message_counter];
-        message_counter <= message_counter + 7'b1;
-        #20
-        PS2_data <= message[message_counter];
-        message_counter <= message_counter + 7'b1;
-        #20
-        PS2_data <= message[message_counter];
-        message_counter <= message_counter + 7'b1;
-        #20
-        PS2_data <= message[message_counter];
-        message_counter <= message_counter + 7'b1;
-        #20
-        PS2_data <= message[message_counter];
-        message_counter <= message_counter + 7'b1;
-        #20
-        PS2_data <= message[message_counter];
-        message_counter <= message_counter + 7'b1;
-        #20
-        PS2_data <= message[message_counter];
-        message_counter <= message_counter + 7'b1;
-
-        #20
-        PS2_data <= odd_pairity(message[message_counter:message_counter-7]);
-
-        #20
-        PS2_data <= 1'b1;
-
-    end
-
-function odd_pairity;
-    input [7:0] in;
-    odd_pairity = in[7] ^ in[6] ^ in[5] ^ in[4] ^ in[3] ^ in[2] ^ in[1] ^ in[0];
-endfunction
 
 endmodule
